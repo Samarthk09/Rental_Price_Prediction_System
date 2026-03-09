dba91cb9840ab8af71118106e110fcba824f599c
Samarth Kolhe <samarthkolhe09@gmail.com>
2026-03-10 00:20:47 +0530

    Project Created

---
+++ rental-price-prediction/src/main/java/com/rental/rental_price_prediction/RentalPricePredictionApplication.java
@@ -0,0 +1,13 @@
+package com.rental.rental_price_prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalPricePredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalPricePredictionApplication.class, args);
+	}
+
+}
---
+++ rental-price-prediction/src/test/java/com/rental/rental_price_prediction/RentalPricePredictionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental.rental_price_prediction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalPricePredictionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
